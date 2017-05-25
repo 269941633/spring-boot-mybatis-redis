ae4e41fb05b6d3fd7b48b660b9cb47585b2bc9a1
Jianfei Wei <269941633@qq.com>
2017-05-25 16:02:37 +0800

    加上druid监控访问配置

--- src/main/java/com/fei/springboot/config/mybatis/DatasourceConfig.java
+++ src/main/java/com/fei/springboot/config/mybatis/DatasourceConfig.java
@@ -7,11 +7,15 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 public class DatasourceConfig {
@@ -68,7 +72,17 @@ public class DatasourceConfig {
     @Value("${spring.datasource.filters}")
     private String filters;
     
+    @Value("${spring.datasource.connectionProperties}")
+    private String connectionProperties;
     
+    @Value("${spring.datasource.useGlobalDataSourceStat}")
+    private boolean useGlobalDataSourceStat;
+    
+    @Value("${spring.datasource.druidLoginName}")
+    private String druidLoginName;
+	
+    @Value("${spring.datasource.druidPassword}")
+    private String druidPassword;
 	
     @Bean(name="dataSource",destroyMethod = "close", initMethod="init")
     @Primary //不要漏了这
@@ -97,4 +111,31 @@ public class DatasourceConfig {
         }  
         return datasource;  
     }
+    
+    /////////  下面是druid 监控访问的设置  /////////////////
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+	    ServletRegistrationBean reg = new ServletRegistrationBean();
+	    reg.setServlet(new StatViewServlet());
+	    reg.addUrlMappings("/druid/*");  //url 匹配
+	    reg.addInitParameter("allow", "192.168.16.110,127.0.0.1"); // IP白名单 (没有配置或者为空，则允许所有访问)
+	    reg.addInitParameter("deny", "192.168.16.111"); //IP黑名单 (存在共同时，deny优先于allow)
+	    reg.addInitParameter("loginUsername", this.druidLoginName);//登录名
+	    reg.addInitParameter("loginPassword", this.druidPassword);//登录密码
+	    reg.addInitParameter("resetEnable", "false"); // 禁用HTML页面上的“Reset All”功能
+	    return reg;
+    }
+
+    @Bean(name="druidWebStatFilter")
+    public FilterRegistrationBean filterRegistrationBean() {
+	    FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+	    filterRegistrationBean.setFilter(new WebStatFilter());
+	    filterRegistrationBean.addUrlPatterns("/*");
+	    filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*"); //忽略资源
+	    filterRegistrationBean.addInitParameter("profileEnable", "true");
+	    filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+	    filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+	    return filterRegistrationBean;
+    }
+   
 }
